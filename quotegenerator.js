996ec30e3de8898d2aeaab76629e1fc5b63c40ee
ktewoj <65322617+ktewoj@users.noreply.github.com>
2020-05-13 19:50:27 -0700

    Add files via upload

---
+++ quotegenerator.js
@@ -0,0 +1,68 @@
+/* Each quote object in the quotes array should have the following properties:
+quote - a string containing the text of the quote that will be displayed on the page.
+source - a string containing the creator of the quote. For example: "Mark Twain" or "Traditional Irish proverb” or "Anonymous".
+citation - optional - a string identifying where the quote comes from, like a speech, publication or a movie. If there is no known publication, do not include this property on the object.
+year - optional - a number identifying the year of the quote. If there is no known year, then do not include this property on the object.
+*/
+
+const quotes = [
+  {
+    quote: "I was the world's nicest guy and they ruined my life for no reason.",
+    source: "Kuzco",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "I've been turned into a cow, can I go home?",
+    source: "Man who has been turned into a cow",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "Fifteen years later, and I still watch The Emperor's New Groove at least once a month.",
+    source: "Martha Stewart",
+    year: 2015,
+  },
+  {
+    quote: "It's not the first time I was tossed out of a window, and it won't be the last.",
+    source: "Old man",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "I'm sorry, but you've thrown off the Emperor's groove.",
+    source: "Random guard",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "Maybe you should've thought of THAT before you became PEASANTS.",
+    source: "Yzma",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "What is this, some little game you country folk like to play?",
+    source: "Kuzco",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "The Emperor's New Groove is an incredible film.",
+    source: "Katie Wojciechowski",
+    citation: "The Mind of Katie Wojciechowski",
+    year: 2020,
+  },
+  {
+    quote: "Let's take a look-see. Hate your hair. Not likely. Yikes. Yikes. Yikes. And, let me guess, you have a great personality.",
+    source: "Kuzco",
+    citation: "The Emperor's New Groove",
+    year: 2000,
+  },
+  {
+    quote: "The Emperor's New Groove: A Cinematic Triumph",
+    source: "President Barack Obama",
+    citation: "Rolling Stone",
+    year: 2010,
+  },
+];
